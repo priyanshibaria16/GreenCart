7fd98d1a12039bb53c70226c4d9af9378340484d
priyanshibaria16 <priyanshibaria05@gmail.com>
2025-09-03 19:18:48 +0530

    navbar

---
+++ Client/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isseller, setIsseller] = useState(false);
+
+  const value = { navigate, user, setUser, isseller, setIsseller };
+
+  return (
+    <AppContext.Provider value={value}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
